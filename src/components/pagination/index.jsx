f7eb4dc667a51c906510385d08096726501d8769
titan <phillreeder@gmail.com>
2018-12-20 12:38:39 +0000

    basic Structure

---
+++ src/components/pagination/index.jsx
@@ -0,0 +1,3 @@
+import Pagination from './searchInput'
+
+export default Pagination
---
+++ src/components/searchInput/searchInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchInput = () => (
+    <div>
+        <input type="text" name="query" placeholder="type your query here"></input>
+        <button>Search</button>
+    </div>
+)
+
+export default SearchInput;
